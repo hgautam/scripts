c10a3132c02eaec1900dce8fc451935c4752f7e5
Gautam,Himanshu(hgautam) <hgautam@ebay.com>
2017-10-25 10:50:27 -0700

    Update curlTest.pl

--- curlTest.pl
+++ curlTest.pl
@@ -2,7 +2,7 @@
 use strict;
 use warnings;
 
-my $timeout = 1200
+my $timeout = 1200;
 my $time = `curl -s -u RaptorTeam:RaptorTeam  -w '%{time_total}\n' --upload-file ims-web-2.8.1-20170915.010455-1.war http://ebaycentral.qa.ebay.com/content/repositories/snapshots/com/ebay/scm/wartest/1.0/ims-web-2.8.1-20170915.010455-1.war`;
 print "Time is $time\n";
 
