0e7490ebe0c309e008c5132e2e1ae251823ac705
Gautam,Himanshu(hgautam) <hgautam@ebay.com>
2017-10-25 10:42:02 -0700

    Update curlTest.pl

--- curlTest.pl
+++ curlTest.pl
@@ -2,10 +2,11 @@
 use strict;
 use warnings;
 
+my $timeout = 1200
 my $time = `curl -s -u RaptorTeam:RaptorTeam  -w '%{time_total}\n' --upload-file ims-web-2.8.1-20170915.010455-1.war http://ebaycentral.qa.ebay.com/content/repositories/snapshots/com/ebay/scm/wartest/1.0/ims-web-2.8.1-20170915.010455-1.war`;
 print "Time is $time\n";
 
-if ($time > 1200) {
+if ($time > $timeout) {
 	print "Build is talking too long!!!\n";
 	exit 1;
 }
