ccdd4ac542a681510c2817a373af455e5c30c8a4
Himanshu Gautam <hgautam@ebay.com>
2017-11-14 11:16:43 -0800

    refactored curlTest.pl

--- curlTest.pl
+++ curlTest.pl
@@ -4,12 +4,18 @@ use warnings;
 
 my $timeout = 1200;
 
+#print "Number of args is $#ARGV\n";
+# Read command line args
 if ( $#ARGV != 1 ) {
-	die "Need to pass ebaycentral hostname as argument!!";
+	print "Need to pass ebaycentral hostname and file name as argument!!\n";
+	print "e.g. curlTest.pl ebaycentral.qa.ebay.com ims-web-2.8.1-20170915.010455-1.war\n";
+	exit 1;
 }
 
 print "$ARGV[0]\n";
-my $time = `curl -s -u RaptorTeam:RaptorTeam  -w '%{time_total}\n' --upload-file ims-web-2.8.1-20170915.010455-1.war http://$ARGV[0]/content/repositories/snapshots/com/ebay/scm/wartest/1.0/ims-web-2.8.1-20170915.010455-1.war`;
+print "$ARGV[1]\n";
+#ims-web-2.8.1-20170915.010455-1.war is the file that is currently being uploded
+my $time = `curl -s -u RaptorTeam:RaptorTeam  -w '%{time_total}\n' --upload-file ims-web-2.8.1-20170915.010455-1.war http://$ARGV[0]/content/repositories/snapshots/com/ebay/scm/wartest/1.0/$ARGV[1]`;
 print "Time is $time\n";
 
 if ($time > $timeout) {
