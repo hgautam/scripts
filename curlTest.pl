820fa2064e5829d5da1729772412a297865271ce
Himanshu Gautam <hgautam@ebay.com>
2017-11-14 11:51:25 -0800

    capture exit code of curl

--- curlTest.pl
+++ curlTest.pl
@@ -16,6 +16,14 @@ print "$ARGV[0]\n";
 print "$ARGV[1]\n";
 #ims-web-2.8.1-20170915.010455-1.war is the file that is currently being uploded
 my $time = `curl -s -u RaptorTeam:RaptorTeam  -w '%{time_total}\n' --upload-file ims-web-2.8.1-20170915.010455-1.war http://$ARGV[0]/content/repositories/snapshots/com/ebay/scm/wartest/1.0/$ARGV[1]`;
+my $extcode = $?>>8;
+
+if ($extcode != 0) {
+	print "exit code is $extcode\n";
+	print "curl command failed\n";
+	exit 1;
+}
+
 print "Time is $time\n";
 
 if ($time > $timeout) {
