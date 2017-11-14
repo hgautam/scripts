05d2bd4afd71c44fd0af4ed45818889bbea3abfe
Himanshu Gautam <hgautam@ebay.com>
2017-11-14 10:50:19 -0800

    parameterized hostname

--- curlTest.pl
+++ curlTest.pl
@@ -3,7 +3,13 @@ use strict;
 use warnings;
 
 my $timeout = 1200;
-my $time = `curl -s -u RaptorTeam:RaptorTeam  -w '%{time_total}\n' --upload-file ims-web-2.8.1-20170915.010455-1.war http://ebaycentral.qa.ebay.com/content/repositories/snapshots/com/ebay/scm/wartest/1.0/ims-web-2.8.1-20170915.010455-1.war`;
+
+if ( $#ARGV != 1 ) {
+	die "Need to pass ebaycentral hostname as argument!!";
+}
+
+print "$ARGV[0]\n";
+my $time = `curl -s -u RaptorTeam:RaptorTeam  -w '%{time_total}\n' --upload-file ims-web-2.8.1-20170915.010455-1.war http://$ARGV[0]/content/repositories/snapshots/com/ebay/scm/wartest/1.0/ims-web-2.8.1-20170915.010455-1.war`;
 print "Time is $time\n";
 
 if ($time > $timeout) {
