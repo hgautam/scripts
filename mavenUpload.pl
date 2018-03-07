f08bbee2d9861412e6a2b7bb7bbbea10a55c317b
Gautam,Himanshu(hgautam) <hgautam@ebay.com>
2018-03-07 11:39:41 -0800

    Update mavenUpload.pl

--- mavenUpload.pl
+++ mavenUpload.pl
@@ -18,7 +18,7 @@ print "ebaycentral host is: $ARGV[0]\n";
 print "file to upload is: $ARGV[1]\n";
 print "dir to upload the file in is: $ARGV[2]\n";
 #ims-web-2.8.1-20170915.010455-1.war is the file that is currently being uploded
-my $time = `curl -s -u RaptorTeam:RaptorTeam  -w '%{time_total}\n' --upload-file $ARGV[1] https://$ARGV[0]/content/repositories/snapshots/com/ebay/scm/$ARGV[2]/1.0/$ARGV[1]`;
+my $time = `curl -s -u RaptorTeam:RaptorTeam  -w '%{time_total}\n' --upload-file $ARGV[1] http://$ARGV[0]/content/repositories/snapshots/com/ebay/scm/$ARGV[2]/1.0/$ARGV[1]`;
 # capture the error code of above command. Very important or script will still 0 code even if the curl command failed due to file not found error
 my $extcode = $?>>8;
 
